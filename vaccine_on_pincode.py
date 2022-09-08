4eef861cce0025de8ae68ad8a2109e6249d510f0
python45845845 <113008059+python45845845@users.noreply.github.com>
2022-09-08 16:59:03 +0530

    Update vaccine_on_pincode.py

--- vaccine_on_pincode.py
+++ vaccine_on_pincode.py
@@ -1,6 +1,5 @@
 from re import L
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import requests
