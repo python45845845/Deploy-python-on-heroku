412d3ba370f90841cef059ef68504bbf6dc6b4bc
python45845845 <113008059+python45845845@users.noreply.github.com>
2022-09-08 17:15:49 +0530

    Update vaccine_on_pincode.py

--- vaccine_on_pincode.py
+++ vaccine_on_pincode.py
@@ -13,7 +13,13 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+options = Options()
+options.binary_location = GOOGLE_CHROME_BIN
 options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.headless = True
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
 driver.get("https://glauniversity.in:8070/LeadManagement/ShowResult")
 s,e = 9080060,9080070
 for i in range(s,e):
