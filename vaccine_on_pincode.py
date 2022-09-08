0621f9a01498421221293a13b229dfb357a1ae2f
python45845845 <113008059+python45845845@users.noreply.github.com>
2022-09-08 16:49:20 +0530

    Update vaccine_on_pincode.py

--- vaccine_on_pincode.py
+++ vaccine_on_pincode.py
@@ -1,16 +1,42 @@
+from re import L
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import requests
-tele_auth_token = "5672053615:AAGRLsunpPWBSpldtOG32Gle_C_WTfrgNYo" # Authentication token provided by Telegram bot
-tel_group_id = "himnandiandu"  
-msg = f"msg reci atubu "
-def send_msg_on_telegram(msg):
-    telegram_api_url = f"https://api.telegram.org/bot{tele_auth_token}/sendMessage?chat_id=@{tel_group_id}&text={msg}"
-    tel_resp = requests.get(telegram_api_url)
-
-    if tel_resp.status_code == 200:
-        print ("Notification has been sent on Telegram")
-    else:
-        print ("Could not send Message")
-
-send_msg_on_telegram(msg)
-
-
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.chrome.options import Options
+import os
+CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
+GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
+options = Options()
+options.binary_location = GOOGLE_CHROME_BIN
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.headless = True
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
+driver.get("https://glauniversity.in:8070/LeadManagement/ShowResult")
+s,e = 9080060,9080070
+for i in range(s,e):
+    a = 'GLA2022-1'+str(i).zfill(7)
+    driver.find_element(By.XPATH,"//input[contains(@id,'reg')]").send_keys(a)
+    driver.find_element(By.XPATH,"//input[contains(@class,'btn')]").click()
+    driver.implicitly_wait(0.025)
+    try :
+        s = driver.find_element(By.XPATH,"//div[normalize-space()='Sorry! Registration Number Not Found']")
+        print (s.txt )
+    except :
+        try :
+            rgnames = driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/h4[1]")
+            rehm = rgnames.text
+            def send_msg_on_telegram(rehm):
+                telegram_api_url = f"https://api.telegram.org/bot5672053615:AAGRLsunpPWBSpldtOG32Gle_C_WTfrgNYo/sendMessage?chat_id=@himnandiandu&text={rehm}{a}"
+                tel_resp = requests.get(telegram_api_url)
+                if tel_resp.status_code == 200:
+                    print ("Notification has been sent on Telegram")
+                else:
+                    print ("Could not send Message")
+            send_msg_on_telegram(rehm)
+        except :
+            pass
+    driver.find_element(By.XPATH,"//input[contains(@id,'reg')]").clear()
