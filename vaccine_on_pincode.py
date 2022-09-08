65151214a0178533353e309bbab08ca61a4e9dc5
python45845845 <113008059+python45845845@users.noreply.github.com>
2022-09-08 15:48:45 +0530

    Update vaccine_on_pincode.py

--- vaccine_on_pincode.py
+++ vaccine_on_pincode.py
@@ -1,26 +1,16 @@
-
-""" 
-Developer: aipython on [29-05-2021]
-website: www.aipython.in
-
-Sends Notifications on a Telegram channel , whenever the Vaccine(s) is available at the given PINCODE 
-"""
-
 import requests
-from datetime import datetime, timedelta
-import time
-import pytz
-
 tele_auth_token = "5672053615:AAGRLsunpPWBSpldtOG32Gle_C_WTfrgNYo" # Authentication token provided by Telegram bot
-tel_group_id = "himnandiandu"          # Telegram group name
-header = {'User-Agent': 'Chrome/84.0.4147.105 Safari/537.36'} # Header for using cowin api
-
+tel_group_id = "himnandiandu"  
+msg = f"msg reci atubu "
 def send_msg_on_telegram(msg):
-    telegram_api_url = f"https://api.telegram.org/bot{tele_auth_token}/sendMessage?chat_id=@{tel_group_id}&text={'hi'}"
+    telegram_api_url = f"https://api.telegram.org/bot{tele_auth_token}/sendMessage?chat_id=@{tel_group_id}&text={msg}"
     tel_resp = requests.get(telegram_api_url)
 
     if tel_resp.status_code == 200:
         print ("Notification has been sent on Telegram")
     else:
         print ("Could not send Message")
+
+send_msg_on_telegram(msg)
+
 
